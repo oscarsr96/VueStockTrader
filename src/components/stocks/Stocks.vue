cb303196f74c5c2f1f645feeb390d2e47866276f
Sanchez Oscar <sanchez.oscar@bcg.com>
2020-09-01 17:01:55 +0200

    Stock components

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-	<h1> The Stock Component</h1>
+	<div>
+		<app-stock v-for="stock in stocks"></app-stock>
+	</div>
 </template>
+
+<script>
+	import Stock from './Stock.vue';
+	export default {
+		data(){
+			return {
+				stocks: [
+					{id: 1, name: "BW", price: 110},
+					{id: 2, name: "FB", price: 120},
+					{id: 3, name: "TW", price: 95},
+					{id: 4, name: "AZ", price: 100},
+
+				]
+			}
+		},
+		components: {
+			appStock: Stock
+		}
+	}
+</script>
